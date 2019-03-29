0347548eba9bb9d30f079d7eaca2de2bc5276b4b
Rajeev Reddy <rajeevr69@yahoo.com>
2019-03-28 18:06:17 -0700

    Update app.js
    Added a guess count limit of 10.

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ guessButton.addEventListener("click", (event)=> {
     document.querySelector("input").disabled=true;
     document.querySelector("input").style.backgroundColor="#98fb98";//green backgroundcolor
     event.preventDefault();
+  } else if (guessCount >= 10) {
+    document.querySelector("h2").style.color="red";
+    headlineTwo.textContent =
+      `You have exceeded your number of guesses. Press the Restart Game Button to start over.`;
+    document.querySelector("input").style.backgroundColor="#ff7f7f";//red backgroundcolor
+    document.querySelector("input").disabled=true;
+    event.preventDefault();
   } else if (input.value % 1 != 0|| isNaN(input.value) || input.value < 1 || input.value > 100){
     document.querySelector("h2").style.color="red";
     headlineTwo.textContent = `Please enter a whole number between 1-100. Not ${input.value}`;
