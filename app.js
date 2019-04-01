033b877ffa8a87e965aa580471e805ea39537785
Rajeev Reddy <rajeevr69@yahoo.com>
2019-03-31 18:11:15 -0700

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ guessButton.addEventListener("click", (event)=> {
     document.querySelector("input").style.backgroundColor = "#ff7f7f";//red backgroundcolor
     document.querySelector("input").disabled = true;
     event.preventDefault();
-  } else if (input.value % 1 != 0|| isNaN(input.value) || input.value < 1 || input.value > 100){
+  } else if (input.value % 1 != 0 || isNaN(input.value) || input.value < 1 || input.value > 100){
     document.querySelector("h2").style.color = "red";
     headlineTwo.textContent = `Please enter a whole number between 1-100. Not ${input.value}`;
     document.querySelector("input").style.backgroundColor = "#ff7f7f";//red backgroundcolor
